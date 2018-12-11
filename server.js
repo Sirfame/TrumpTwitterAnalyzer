5ddbda9cbb1bf23f93fb06fda023c35a17fbad2e
Sirfame Lin <sirfame@hotmail.com>
2018-12-11 06:28:29 -0800

    sending redis response to /, but not scalable with KEYS

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ const app = express();
 //////////////////////////////////
 
 var filteredTweets = {}
+var scoredTweets = {};
+
+
+var redisClient = redis.createClient({
+    port : global.gConfig.redis_config_values.port,
+    host : global.gConfig.redis_config_values.host,
+    password : global.gConfig.redis_config_values.password
+});
+
 getTweets().then(function(tweets) {
     console.log('///////////////// Calling Redis')
     console.log(tweets)
@@ -40,25 +49,32 @@ getTweets().then(function(tweets) {
 }).then(function(jsonScores) {
     console.log('/////////////////: Adding to Redis')
     console.log(jsonScores)
-    var redisClient = redis.createClient({
-        port : global.gConfig.redis_config_values.port,
-        host : global.gConfig.redis_config_values.host,
-        password : global.gConfig.redis_config_values.password
-    });
+
     redisClient.on('connect', function() {
         console.log('Redis client connected');
     });   
     for(var i = 0; i < jsonScores['documents'].length; i++) {
         console.log("jsonscoreID" + jsonScores['documents'][i]['id']);
-        redisClient.set(jsonScores['documents'][i]['id'], JSON.stringify({
+        var newRecord = JSON.stringify({
             "score" : jsonScores['documents'][i]['score'],
             "text" : filteredTweets[jsonScores['documents'][i]['id']]['text'],
             "id" : jsonScores['documents'][i]['id']
-        }), redis.print)
+        });
+        scoredTweets[jsonScores['documents'][i]['id']] = newRecord;
+        redisClient.set(jsonScores['documents'][i]['id'], newRecord, redis.print)
     }
 }).catch(function(error) {
     console.log("Error: " + error);
 });
+
+app.get('/', function(req, res) {
+    redisClient.keys('*', function(err, keys) {
+        for(var i = 0; i < keys.length; i++) {
+            
+        }
+        res.send(keys)
+    })
+})
 
 app.listen(global.gConfig.node_port, () => {
     console.log(`${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`);
