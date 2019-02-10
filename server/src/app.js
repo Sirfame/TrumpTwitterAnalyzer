b8a7a6ca1f52b121ba76076d58828c0b96f0abc4
Sirfame Lin <sirfame@hotmail.com>
2019-02-09 17:01:49 -0800

    able to promisfy redis finally

--- server/src/app.js
+++ server/src/app.js
@@ -6,8 +6,11 @@ const redis = require("redis")
 const bluebird = require('bluebird')
     
 
+
+bluebird.promisifyAll(redis);
 const app = express()
 const redisClient = redis.createClient();
+
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -19,7 +22,33 @@ redisClient.on("error", function (err) {
 
 redisClient.on('connect', function() {
   console.log('Redis client connected');
-}); 
+});
+
+var redisCall = function(client) {  
+  return new Promise(function(resolve, reject) {      
+      client.get()
+  
+  });
+}
+
+app.get('/tweets', (req, res) => {
+  res.send('test');
+});
+
+function getFromRedis(client, key) {
+  return new Promise(function(resolve, reject) {
+    redisClient.keys('*', function (err, keys) {
+      if(err) reject(err);
+      var tweets = [];
+      for(key in keys) {
+        
+      }
+
+
+
+    });
+  });
+}
 
 app.get('/posts', (req, res) => {
   // res.send(
@@ -28,13 +57,23 @@ app.get('/posts', (req, res) => {
   //     description: "Hi there! How are you?"
   //   }]
   // )
+  var tweets = [];  
   redisClient.keys('*', function (err, keys) {
-    res.send(
-      [{
-        title: keys
-      }]
-    )
-  })
+    if(err) console.error(err)
+
+    for(var i = 0; i < keys.length; i++) {
+      console.log(keys[i])
+      redisClient.getAsync(keys[i]).then(JSON.parse).then( function(val) {
+        console.log(val.text);
+      })
+    }
+    res.send([{
+      title: keys
+    }]);
+  
+    
+    
+  });
 });  
 
 app.listen(process.env.PORT || 8081)
